2e77135d8fad71d57eef057c6979243e1adbde37
mo1ein <moeinn.com@gmail.com>
2022-09-11 02:11:55 +0430

    Refactor cmd

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,19 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/mo1ein/makhzan"
+	"github.com/mo1ein/makhzan/makhzan"
 )
 
 func Execute() {
-	fmt.Println("Welcome to makhzan :)\n")
+	fmt.Println("Welcome to makhzan :)")
 	client, ctx := makhzan.GhAuth()
+
 	// not forked repos
-	allRepos := makhzan.RepoList(client, ctx)
+	allRepos, err := makhzan.RepoList(client, ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(allRepos)
 	allLangs := makhzan.LangList(client, ctx, allRepos, "mo1ein")
 	makhzan.PieChart(allLangs, "Ratio of languages in github repo")
 }
